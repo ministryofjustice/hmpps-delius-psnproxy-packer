ca126eb265279015543b5389ab78ae27f7fa6836
Paul Crimes <paul.crimes@solirius.com>
2020-08-19 17:13:31 +0100

    install awslogs libraries as part of packer build

---
+++ scripts/install_awslogs.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+sudo -i
+
+#create aws logs directories as installer does not
+mkdir -p  /etc/awslogs
+mkdir -p /var/awslogs/state/
+
+#download installer
+curl https://s3.amazonaws.com/aws-cloudwatch/downloads/latest/awslogs-agent-setup.py -O
+
+#create dummy config
+cat > /tmp/awslogs.conf <<- EOF
+[general]
+state_file = /var/awslogs/state/agent-state
+EOF
+
+#run installer with dummy config, to be replaced on ec2 autostart up bootstrap
+python ./awslogs-agent-setup.py --region eu-west-2 -n -c /tmp/awslogs.conf
+
