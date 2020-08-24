9c600744fc69d1d92c7623dbf654d525f53001a2
Paul Crimes <paul.crimes@solirius.com>
2020-08-24 17:09:37 +0100

    WIP

--- scripts/initialise_runtime_scripts_directories.sh
+++ scripts/initialise_runtime_scripts_directories.sh
@@ -1,2 +1,6 @@
 #!/usr/bin/env bash
-chown -R centos /opt/hmpps
+mkdir -p /opt/hmpps/proxy-scripts/
+PATH=/opt/hmpps/proxy-scripts/:$PATH
+PATH=/opt/hmpps/proxy-scripts/pre-prod:$PATH
+PATH=/opt/hmpps/proxy-scripts/production:$PATH
+PATH=/opt/hmpps/proxy-scripts/stage:$PATH
