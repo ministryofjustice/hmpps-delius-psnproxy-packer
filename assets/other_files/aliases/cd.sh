43c6b0ccdbffa4ab3905fda4897278db8231bfde
Paul Crimes <paul.crimes@solirius.com>
2020-09-24 16:02:19 +0100

    fix bahrd escaping

--- assets/other_files/aliases/cd.sh
+++ assets/other_files/aliases/cd.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
 alias .cd.config='cd /etc/nginx/conf.d/'
+alias .cd.log='cd /var/log/nginx'
--- scripts/20_configure_server.sh
+++ scripts/20_configure_server.sh
@@ -8,7 +8,7 @@ ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
 
 
 #update bash profile with paths and aliases
-cat << EOF >> /root/.bashrc
+cat << 'EOF' >> /root/.bashrc
 PATH=/opt/hmpps/proxy-scripts/:$PATH
 PATH=/opt/hmpps/proxy-scripts/pre-prod:$PATH
 PATH=/opt/hmpps/proxy-scripts/production:$PATH
@@ -16,7 +16,7 @@ PATH=/opt/hmpps/proxy-scripts/stage:$PATH
 
 #source aliases
 for f in /opt/hmpps/aliases/*.sh; do
-   source "$$f" -H
+   source "$f" -H
 done
 
 
