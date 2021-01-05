63f49654baba106c1baaf83ee8314e7e46d21747
Chris Kinsella <chris.kinsella@helkinsoft.com>
2021-01-05 17:06:41 +0000

    added letsencrypt certbot install and im-proxy confs

---
+++ scripts/40_install_letsencrypt.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+sudo -i
+
+# install letsencrypt certbot-nginx
+echo '*** installing epel via amazon-linux-extras..'
+sudo amazon-linux-extras install -y epel 
+
+echo '*** yum update..'
+sudo yum update -y
+
+echo '*** installing certbot-nginx..'
+sudo yum install -y certbot-nginx
+
+# copy config file for psn cert.
+cp /tmp/assets/letsencrypt/psn.probation.service.justice.gov.uk.conf /etc/letsencrypt/renewal/psn.probation.service.justice.gov.uk.conf
+
