953a541f3558d4ac53d203e149188c3200c4581f
Chris Kinsella <chris.kinsella@helkinsoft.com>
2021-01-05 18:13:49 +0000

    added reinstall packages to fix python error

--- scripts/40_install_letsencrypt.sh
+++ scripts/40_install_letsencrypt.sh
@@ -2,13 +2,15 @@
 
 sudo -i
 
+
+yum reinstall -y ca-certificates python-urllib3 python-requests pyOpenSSL
+
 # install letsencrypt certbot-nginx
 sudo yum install -y epel-release
 sudo yum install -y certbot-nginx
 sudo yum install -y tree
 
-cd /etc/letsencrypt
-tree
+certbot --help
 
 # copy config file for psn cert.
 # cp /tmp/assets/letsencrypt/renewal/psn.probation.service.justice.gov.uk.conf /etc/letsencrypt/renewal/psn.probation.service.justice.gov.uk.conf
