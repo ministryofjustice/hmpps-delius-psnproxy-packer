bdc803c13b8bd00a7e4c431fe80251fac87d3fd1
Chris Kinsella <chris.kinsella@helkinsoft.com>
2021-01-06 13:23:05 +0000

    updated python libs

--- scripts/40_install_letsencrypt.sh
+++ scripts/40_install_letsencrypt.sh
@@ -3,15 +3,20 @@
 sudo -i
 
 # remove pip installed urllib3 as it conflicts with certbot
-sudo pip uninstall urllib3 -y
-
-
-yum reinstall -y ca-certificates python-urllib3 python-requests pyOpenSSL
+#sudo pip uninstall urllib3 -y
+#sudo yum reinstall -y ca-certificates python-urllib3 python-requests pyOpenSSL
+#sudo yum install -y python-cryptography
 
 # install letsencrypt certbot-nginx
 sudo yum install -y epel-release
 sudo yum install -y certbot-nginx
+
 sudo yum install -y tree
+
+sudo pip uninstall pyopenssl
+sudo pip uninstall cryptography
+sudo pip install pyopenssl
+sudo pip install cryptography
 
 certbot --help
 
