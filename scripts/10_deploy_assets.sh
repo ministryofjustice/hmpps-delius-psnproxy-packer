3c9742be6b7e7de109087698dc38b90458038281
Chris Kinsella <chris.kinsella@helkinsoft.com>
2021-01-07 12:45:00 +0000

    commented out scripts - debugging AMI

--- scripts/10_deploy_assets.sh
+++ scripts/10_deploy_assets.sh
@@ -3,7 +3,7 @@ mkdir -p /opt/hmpps/proxy_scripts/
 mv /tmp/assets/runtime_helper_scripts/* /opt/hmpps/proxy_scripts
 mv /tmp/assets/other_files/aliases /opt/hmpps
 
-chown -R centos /opt/hmpps
+chown -R ec2-user /opt/hmpps
 
 
 
