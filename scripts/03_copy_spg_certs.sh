8168c9d411d2f48817493f110f990526dcfdafb1
Chris Kinsella <chris.kinsella@helkinsoft.com>
2021-01-21 15:06:21 +0000

    updated path to correct cert and certs copy script

--- scripts/03_copy_spg_certs.sh
+++ scripts/03_copy_spg_certs.sh
@@ -5,12 +5,12 @@ mkdir -p /etc/pki/tls/private
 
 # delius-prod - spg-psn-ext-tls
 aws s3 cp s3://tf-eu-west-2-hmpps-eng-dev-certificates-private-s3bucket/official-data/hmpps-delius-prod/current/haproxy/ca.pem	/etc/pki/tls/certs/spgw-int-psn.probation.service.justice.gov.uk.public-certificate.pem
-aws s3 cp s3://tf-eu-west-2-hmpps-eng-dev-certificates-private-s3bucket/official-data/hmpps-delius-prod/current/spg-psn-ext-tls/spgw-ext-psn.probation.service.justice.gov.uk.public-certificate.pem /etc/pki/tls/private/spgw-int-psn.probation.service.justice.gov.uk.key
-aws s3 cp s3://tf-eu-west-2-hmpps-eng-dev-certificates-private-s3bucket/official-data/hmpps-delius-prod/current/spg-psn-ext-tls/spgw-ext-psn.probation.service.justice.gov.uk.key /etc/pki/tls/certs/privateca.probation.service.justice.gov.uk.public-certificate.pem
+aws s3 cp s3://tf-eu-west-2-hmpps-eng-dev-certificates-private-s3bucket/official-data/hmpps-delius-prod/current/spg-psn-int-tls/spgw-int-psn.probation.service.justice.gov.uk.key /etc/pki/tls/private/spgw-int-psn.probation.service.justice.gov.uk.key
+aws s3 cp s3://tf-eu-west-2-hmpps-eng-dev-certificates-private-s3bucket/official-data/hmpps-delius-prod/current/private_ca/privateca.probation.service.justice.gov.uk.public-certificate.pem /etc/pki/tls/certs/privateca.probation.service.justice.gov.uk.public-certificate.pem
 
 # delius-prod - spg-psn-int-tls
-aws s3 cp s3://tf-eu-west-2-hmpps-eng-dev-certificates-private-s3bucket/official-data/hmpps-delius-prod/current/spg-psn-int-tls/spgw-int-psn.probation.service.justice.gov.uk.public-certificate.pem /etc/pki/tls/certs/spgw-ext-psn.probation.service.justice.gov.uk.public-certificate.pem
-aws s3 cp s3://tf-eu-west-2-hmpps-eng-dev-certificates-private-s3bucket/official-data/hmpps-delius-prod/current/spg-psn-int-tls/spgw-int-psn.probation.service.justice.gov.uk.key /etc/pki/tls/private/spgw-ext-psn.probation.service.justice.gov.uk.key
+aws s3 cp s3://tf-eu-west-2-hmpps-eng-dev-certificates-private-s3bucket/official-data/hmpps-delius-prod/current/spg-psn-ext-tls/spgw-ext-psn.probation.service.justice.gov.uk.public-certificate.pem /etc/pki/tls/certs/spgw-ext-psn.probation.service.justice.gov.uk.public-certificate.pem
+aws s3 cp s3://tf-eu-west-2-hmpps-eng-dev-certificates-private-s3bucket/official-data/hmpps-delius-prod/current/spg-psn-int-tls/spgw-ext-psn.probation.service.justice.gov.uk.key /etc/pki/tls/private/spgw-ext-psn.probation.service.justice.gov.uk.key
 aws s3 cp s3://tf-eu-west-2-hmpps-eng-dev-certificates-private-s3bucket/official-data/hmpps-delius-prod/current/private_ca/spg-ca-list-prod-Sep2020.crt /etc/pki/tls/certs/spg-ca-list-prod-Sep2020.crt
 			
 # delius-pre-prod - spgw-ext-psn.pre-prod
@@ -22,3 +22,28 @@ aws s3 cp s3://tf-eu-west-2-hmpps-eng-dev-certificates-private-s3bucket/official
 aws s3 cp s3://tf-eu-west-2-hmpps-eng-dev-certificates-private-s3bucket/official-data/hmpps-delius-pre-prod/current/spg-psn-int-tls/spgw-int-psn.pre-prod.probation.service.justice.gov.uk.public-certificate.pem /etc/pki/tls/certs/spgw-int-psn.pre-prod.probation.service.justice.gov.uk.public-certificate.pem
 aws s3 cp s3://tf-eu-west-2-hmpps-eng-dev-certificates-private-s3bucket/official-data/hmpps-delius-pre-prod/current/spg-psn-int-tls/spgw-int-psn.pre-prod.probation.service.justice.gov.uk.key /etc/pki/tls/private/spgw-int-psn.pre-prod.probation.service.justice.gov.uk.key
 aws s3 cp s3://tf-eu-west-2-hmpps-eng-dev-certificates-private-s3bucket/official-data/hmpps-delius-pre-prod/current/private_ca/privateca.pre-prod.probation.service.justice.gov.uk.public-certificate.pem /etc/pki/tls/certs/privateca.pre-prod.probation.service.justice.gov.uk.public-certificate.pem
+
+
+# ssl_certificate                 /etc/pki/tls/certs/spgw-int-psn.probation.service.justice.gov.uk.public-certificate.pem;
+#   ssl_certificate_key             /etc/pki/tls/private/spgw-int-psn.probation.service.justice.gov.uk.key;
+#   ssl_client_certificate	        /etc/pki/tls/certs/privateca.probation.service.justice.gov.uk.public-certificate.pem;
+
+# ssl_certificate                   /etc/pki/tls/certs/spgw-ext-psn.probation.service.justice.gov.uk.public-certificate.pem;
+#   ssl_certificate_key               /etc/pki/tls/private/spgw-ext-psn.probation.service.justice.gov.uk.key;
+#   ssl_client_certificate	          /etc/pki/tls/certs/spg-ca-list-prod-Sep2020.crt;
+
+
+
+# ssl_certificate                   /etc/pki/tls/certs/spgw-ext-psn.pre-prod.probation.service.justice.gov.uk.public-certificate.pem;
+#   ssl_certificate_key               /etc/pki/tls/private/spgw-ext-psn.pre-prod.probation.service.justice.gov.uk.key;
+#   ssl_client_certificate            /etc/pki/tls/certs/spg-ca-list-pre-prod-and-prod-sep2020.crt;
+
+#  ssl_certificate                   /etc/pki/tls/certs/spgw-int-psn.pre-prod.probation.service.justice.gov.uk.public-certificate.pem;
+#   ssl_certificate_key               /etc/pki/tls/private/spgw-int-psn.pre-prod.probation.service.justice.gov.uk.key;
+#   ssl_client_certificate            /etc/pki/tls/certs/privateca.pre-prod.probation.service.justice.gov.uk.public-certificate.pem;
+
+
+
+
+
+
