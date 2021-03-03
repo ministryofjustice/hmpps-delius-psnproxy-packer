c96ed169f1680363c011ec7378766dfdc5224955
Mark Butler <mark.butler@solirius.com>
2021-03-03 18:32:46 +0000

    Changes to bundle names for consistency and rename of the CA_ROOT and CA_INTER folders to avoid a clash with SPG requirements

--- scripts/03_copy_spg_certs.sh
+++ scripts/03_copy_spg_certs.sh
@@ -11,12 +11,12 @@ aws s3 cp s3://tf-eu-west-2-hmpps-eng-dev-certificates-private-s3bucket/official
 # delius-prod - spg-psn-ext-tls
 aws s3 cp s3://tf-eu-west-2-hmpps-eng-dev-certificates-private-s3bucket/official-data/hmpps-delius-prod/current/spg-psn-ext-tls/spgw-ext-psn.probation.service.justice.gov.uk.public-certificate.pem /etc/pki/tls/certs/spgw-ext-psn.probation.service.justice.gov.uk.public-certificate.pem
 aws s3 cp s3://tf-eu-west-2-hmpps-eng-dev-certificates-private-s3bucket/official-data/hmpps-delius-prod/current/spg-psn-ext-tls/spgw-ext-psn.probation.service.justice.gov.uk.key /etc/pki/tls/private/spgw-ext-psn.probation.service.justice.gov.uk.key
-aws s3 cp s3://tf-eu-west-2-hmpps-eng-dev-certificates-private-s3bucket/official-data/hmpps-delius-prod/current/private_ca/spg-ca-list-prod-Sep2020.crt /etc/pki/tls/certs/spg-ca-list-prod-Sep2020.crt
+aws s3 cp s3://tf-eu-west-2-hmpps-eng-dev-certificates-private-s3bucket/official-data/hmpps-delius-prod/current/private_ca/spg-ca-list-prod.crt /etc/pki/tls/certs/spg-ca-list-prod.crt
 			
 # delius-pre-prod - spgw-ext-psn.pre-prod
 aws s3 cp s3://tf-eu-west-2-hmpps-eng-dev-certificates-private-s3bucket/official-data/hmpps-delius-pre-prod/current/spg-psn-ext-tls/spgw-ext-psn.pre-prod.probation.service.justice.gov.uk.public-certificate.pem /etc/pki/tls/certs/spgw-ext-psn.pre-prod.probation.service.justice.gov.uk.public-certificate.pem
 aws s3 cp s3://tf-eu-west-2-hmpps-eng-dev-certificates-private-s3bucket/official-data/hmpps-delius-pre-prod/current/spg-psn-ext-tls/spgw-ext-psn.pre-prod.probation.service.justice.gov.uk.key /etc/pki/tls/private/spgw-ext-psn.pre-prod.probation.service.justice.gov.uk.key
-aws s3 cp s3://tf-eu-west-2-hmpps-eng-dev-certificates-private-s3bucket/official-data/hmpps-delius-pre-prod/current/private_ca/spg-ca-list-pre-prod-and-prod-sep2020.crt /etc/pki/tls/certs/spg-ca-list-pre-prod-and-prod-sep2020.crt
+aws s3 cp s3://tf-eu-west-2-hmpps-eng-dev-certificates-private-s3bucket/official-data/hmpps-delius-pre-prod/current/private_ca/spg-ca-list-pre-prod.crt /etc/pki/tls/certs/spg-ca-list-pre-prod.crt
 
 # delius-pre-prod - spgw-int-psn.pre-prod
 aws s3 cp s3://tf-eu-west-2-hmpps-eng-dev-certificates-private-s3bucket/official-data/hmpps-delius-pre-prod/current/spg-psn-int-tls/spgw-int-psn.pre-prod.probation.service.justice.gov.uk.public-certificate.pem /etc/pki/tls/certs/spgw-int-psn.pre-prod.probation.service.justice.gov.uk.public-certificate.pem
