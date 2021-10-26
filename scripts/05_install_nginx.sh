dcadd1cc23465187afe11624c015b1c36d90f5b8
enyambudzi <e.nyambudzi@googlemail.com>
2021-10-26 16:44:47 +0100

    update nginx log permissions (#24)

--- scripts/05_install_nginx.sh
+++ scripts/05_install_nginx.sh
@@ -19,6 +19,10 @@ cp /tmp/assets/ssl/ca-bundle.crt /etc/pki/tls/certs/ca-bundle.crt
 
 mkdir -p /etc/nginx/conf.d
 
+# ensure nginx user has permissions to nginx log directory
+chown nginx:nginx /var/log/nginx
+chmod 0700 /var/log/nginx
+
 # nginx core configs
 cp /tmp/assets/nginx_config_files/nginx.conf /etc/nginx/nginx.conf
 cp /tmp/assets/nginx_config_files/proxy_params /etc/nginx/proxy_params
